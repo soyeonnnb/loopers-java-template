792171ce2b416e68c3de3728d2a8457397e756d3
soyeonnnb <la28s5d@naver.com>
2025-07-15 20:55:19 +0900

    test: User Entity 생성 단위 테스트 코드 작성

--- apps/commerce-api/src/main/java/com/loopers/domain/user/UserEntity.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/UserEntity.java
@@ -26,7 +26,7 @@ public class UserEntity extends BaseEntity {
 
     static final Pattern LOGIN_ID_PATTERN = Pattern.compile("^[a-zA-Z0-9]{1,10}$");
     static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
-    static final Pattern BIRTH_PATTERN = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}$");
+    static final Pattern BIRTH_PATTERN = Pattern.compile("^(19|20)\\d\\d-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$");
 
     public UserEntity(String loginId, String password, String email, String name, String nickname, String birthDate, String gender) {
 
