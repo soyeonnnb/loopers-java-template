63edcc0dbfc6675de85f47a61f85ac06bffb39be
soyeonnnb <la28s5d@naver.com>
2025-07-19 22:31:35 +0900

    refactor: login, email, birth 정규식 패턴 private 처리

--- apps/commerce-api/src/main/java/com/loopers/domain/user/UserEntity.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/UserEntity.java
@@ -13,9 +13,9 @@ import java.util.regex.Pattern;
 @Table(name = "user")
 public class UserEntity extends BaseEntity {
 
-    static final Pattern LOGIN_ID_PATTERN = Pattern.compile("^[a-zA-Z0-9]{1,10}$");
-    static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
-    static final Pattern BIRTH_PATTERN = Pattern.compile("^(19|20)\\d\\d-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$");
+    private static final Pattern LOGIN_ID_PATTERN = Pattern.compile("^[a-zA-Z0-9]{1,10}$");
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+    private static final Pattern BIRTH_PATTERN = Pattern.compile("^(19|20)\\d\\d-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$");
     private String loginId;
     private String password;
     private String email;
