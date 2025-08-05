d8cd8272ddd6faf6e4efbd28644161bdd3e987a8
soyeonnnb <la28s5d@naver.com>
2025-08-06 00:38:02 +0900

    test: 사용자 쿠폰 도메인/단위 테스트 코드 작성

--- apps/commerce-api/src/test/java/com/loopers/domain/coupon/UserCouponModelTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/coupon/UserCouponModelTest.java
@@ -28,10 +28,11 @@ public class UserCouponModelTest {
             ZoneId zoneId = ZoneId.of("Asia/Seoul");
             ZonedDateTime expiredAt = ZonedDateTime.of(localDateTime, zoneId);
             Boolean isUsed = false;
+            ZonedDateTime usedAt = null;
 
             // act
             CoreException coreException = assertThrows(CoreException.class, () ->
-                    new UserCouponEntity(user, coupon, expiredAt, isUsed)
+                    new UserCouponEntity(user, coupon, expiredAt, isUsed, usedAt)
             );
 
             // assert
@@ -51,10 +52,11 @@ public class UserCouponModelTest {
             ZoneId zoneId = ZoneId.of("Asia/Seoul");
             ZonedDateTime expiredAt = ZonedDateTime.of(localDateTime, zoneId);
             Boolean isUsed = false;
+            ZonedDateTime usedAt = null;
 
             // act
             CoreException coreException = assertThrows(CoreException.class, () ->
-                    new UserCouponEntity(user, coupon, expiredAt, isUsed)
+                    new UserCouponEntity(user, coupon, expiredAt, isUsed, null)
             );
 
             // assert
@@ -72,10 +74,11 @@ public class UserCouponModelTest {
             CouponEntity coupon = Instancio.create(CouponEntity.class);
             ZonedDateTime expiredAt = null;
             Boolean isUsed = false;
+            ZonedDateTime usedAt = null;
 
             // act
             CoreException coreException = assertThrows(CoreException.class, () ->
-                    new UserCouponEntity(user, coupon, expiredAt, isUsed)
+                    new UserCouponEntity(user, coupon, expiredAt, isUsed, null)
             );
 
             // assert
@@ -95,10 +98,11 @@ public class UserCouponModelTest {
             ZoneId zoneId = ZoneId.of("Asia/Seoul");
             ZonedDateTime expiredAt = ZonedDateTime.of(localDateTime, zoneId);
             Boolean isUsed = null;
+            ZonedDateTime usedAt = null;
 
             // act
             CoreException coreException = assertThrows(CoreException.class, () ->
-                    new UserCouponEntity(user, coupon, expiredAt, isUsed)
+                    new UserCouponEntity(user, coupon, expiredAt, isUsed, usedAt)
             );
 
             // assert
@@ -108,9 +112,33 @@ public class UserCouponModelTest {
             );
         }
 
-        @DisplayName("모든 필드가 잘 작성되면 객체가 생성이 된다.")
+        @DisplayName("사용여부가 true이고, 사용일자가 null이면 객체 생성에 실패한다.")
         @Test
-        void success() {
+        void throwsBadRequestException_whenIsUsedIsTrueAndUsedAtIsNull() {
+            // arrange
+            UserEntity user = Instancio.create(UserEntity.class);
+            CouponEntity coupon = Instancio.create(CouponEntity.class);
+            LocalDateTime localDateTime = LocalDateTime.of(0, 1, 1, 10, 30);
+            ZoneId zoneId = ZoneId.of("Asia/Seoul");
+            ZonedDateTime expiredAt = ZonedDateTime.of(localDateTime, zoneId);
+            Boolean isUsed = true;
+            ZonedDateTime usedAt = null;
+
+            // act
+            CoreException coreException = assertThrows(CoreException.class, () ->
+                    new UserCouponEntity(user, coupon, expiredAt, isUsed, usedAt)
+            );
+
+            // assert
+            assertAll(
+                    () -> assertEquals(GlobalErrorType.BAD_REQUEST, coreException.getErrorType()),
+                    () -> assertEquals("사용여부는 null일 수 없습니다.", coreException.getCustomMessage())
+            );
+        }
+
+        @DisplayName("쿠폰을 사용하지 않았을 때, 모든 필드가 잘 작성되면 객체가 생성이 된다.")
+        @Test
+        void success_whenNotUsed() {
             // arrange
             UserEntity user = Instancio.create(UserEntity.class);
             CouponEntity coupon = Instancio.create(CouponEntity.class);
@@ -118,9 +146,10 @@ public class UserCouponModelTest {
             ZoneId zoneId = ZoneId.of("Asia/Seoul");
             ZonedDateTime expiredAt = ZonedDateTime.of(localDateTime, zoneId);
             Boolean isUsed = false;
+            ZonedDateTime usedAt = null;
 
             // act
-            UserCouponEntity userCouponEntity = new UserCouponEntity(user, coupon, expiredAt, isUsed);
+            UserCouponEntity userCouponEntity = new UserCouponEntity(user, coupon, expiredAt, isUsed, usedAt);
 
             // assert
             assertAll(
@@ -130,7 +159,65 @@ public class UserCouponModelTest {
                     () -> assertNotNull(userCouponEntity.getCoupon()),
                     () -> assertEquals(coupon.getId(), userCouponEntity.getCoupon().getId()),
                     () -> assertEquals(expiredAt, userCouponEntity.getExpiredAt()),
-                    () -> assertEquals(isUsed, userCouponEntity.getIsUsed())
+                    () -> assertEquals(isUsed, userCouponEntity.getIsUsed()),
+                    () -> assertEquals(usedAt, userCouponEntity.getUsedAt()),
+                    () -> assertEquals(usedAt, userCouponEntity.getUsedAt())
+            );
+        }
+
+        @DisplayName("쿠폰을 사용하지 않았을 때, usedAt이 null이 아니어도 Null로 객체가 생성이 된다.")
+        @Test
+        void success_whenNotUsedAndUsedAtIsNotNull() {
+            // arrange
+            UserEntity user = Instancio.create(UserEntity.class);
+            CouponEntity coupon = Instancio.create(CouponEntity.class);
+            LocalDateTime localDateTime = LocalDateTime.of(0, 1, 1, 10, 30);
+            ZoneId zoneId = ZoneId.of("Asia/Seoul");
+            ZonedDateTime expiredAt = ZonedDateTime.of(localDateTime, zoneId);
+            Boolean isUsed = false;
+            ZonedDateTime usedAt = ZonedDateTime.now();
+
+            // act
+            UserCouponEntity userCouponEntity = new UserCouponEntity(user, coupon, expiredAt, isUsed, usedAt);
+
+            // assert
+            assertAll(
+                    () -> assertNotNull(userCouponEntity),
+                    () -> assertNotNull(userCouponEntity.getUser()),
+                    () -> assertEquals(user.getId(), userCouponEntity.getUser().getId()),
+                    () -> assertNotNull(userCouponEntity.getCoupon()),
+                    () -> assertEquals(coupon.getId(), userCouponEntity.getCoupon().getId()),
+                    () -> assertEquals(expiredAt, userCouponEntity.getExpiredAt()),
+                    () -> assertEquals(isUsed, userCouponEntity.getIsUsed()),
+                    () -> assertNull(userCouponEntity.getUsedAt())
+            );
+        }
+
+        @DisplayName("쿠폰을 사용하지 않았을 때, 모든 필드가 잘 작성되면 객체가 생성이 된다.")
+        @Test
+        void success_whenUsed() {
+            // arrange
+            UserEntity user = Instancio.create(UserEntity.class);
+            CouponEntity coupon = Instancio.create(CouponEntity.class);
+            LocalDateTime localDateTime = LocalDateTime.of(0, 1, 1, 10, 30);
+            ZoneId zoneId = ZoneId.of("Asia/Seoul");
+            ZonedDateTime expiredAt = ZonedDateTime.of(localDateTime, zoneId);
+            Boolean isUsed = true;
+            ZonedDateTime usedAt = ZonedDateTime.now();
+
+            // act
+            UserCouponEntity userCouponEntity = new UserCouponEntity(user, coupon, expiredAt, isUsed, usedAt);
+
+            // assert
+            assertAll(
+                    () -> assertNotNull(userCouponEntity),
+                    () -> assertNotNull(userCouponEntity.getUser()),
+                    () -> assertEquals(user.getId(), userCouponEntity.getUser().getId()),
+                    () -> assertNotNull(userCouponEntity.getCoupon()),
+                    () -> assertEquals(coupon.getId(), userCouponEntity.getCoupon().getId()),
+                    () -> assertEquals(expiredAt, userCouponEntity.getExpiredAt()),
+                    () -> assertEquals(isUsed, userCouponEntity.getIsUsed()),
+                    () -> assertEquals(usedAt, userCouponEntity.getUsedAt())
             );
         }
     }
