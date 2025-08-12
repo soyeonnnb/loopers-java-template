cf4bb1d23b1dabb74c984bcb8d614d6af4a67ee2
soyeonnnb <la28s5d@naver.com>
2025-08-13 00:06:42 +0900

    feature: 상품 리스트 조회 API 추가

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/product/ProductV1Controller.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/product/ProductV1Controller.java
@@ -2,9 +2,12 @@ package com.loopers.interfaces.api.product;
 
 import com.loopers.application.product.ProductFacade;
 import com.loopers.application.product.ProductInfo;
+import com.loopers.application.product.ProductSortOrder;
 import com.loopers.interfaces.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,5 +21,17 @@ public class ProductV1Controller implements ProductV1ApiSpec {
     public ApiResponse<ProductV1Dto.ProductResponse> getProductInfo(@RequestHeader(value = "X-USER-ID", required = false) String userId, @PathVariable("productId") Long productId) {
         ProductInfo response = productFacade.getProductInfo(userId, productId);
         return ApiResponse.success(ProductV1Dto.ProductResponse.from(response));
+    }
+
+    @Override
+    @GetMapping
+    public ApiResponse<List<ProductV1Dto.ProductResponse>> getProductList(
+            @RequestHeader(value = "X-USER-ID", required = false) String userId,
+            @RequestParam(name = "brandId", required = false) Long brandId,
+            @RequestParam(name = "order", required = false) ProductSortOrder order,
+            @RequestParam(name = "size", required = false, defaultValue = "20") Integer size,
+            @RequestParam(name = "page", required = false, defaultValue = "0") Integer page) {
+        List<ProductInfo> response = productFacade.getProductInfoList(userId, brandId, order, size, page);
+        return ApiResponse.success(response.stream().map(ProductV1Dto.ProductResponse::from).toList());
     }
 }
